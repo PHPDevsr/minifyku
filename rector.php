4bb930c778db60bd0f42f53e24d0202bd0df538b
Denny Septian Panggabean <97607754+ddevsr@users.noreply.github.com>
2023-09-18 16:25:13 +0700

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of PHPDevsr/Minifyku.
+ *
+ * (c) 2023 Denny Septian Panggabean <xamidimura@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 use Rector\CodeQuality\Rector\Assign\CombinedAssignRector;
 use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
 use Rector\CodeQuality\Rector\BooleanNot\ReplaceMultipleBooleanNotRector;
