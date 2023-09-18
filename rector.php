77339ed24ed041fce074e1255f6af1006a2d3232
Denny Septian Panggabean <97607754+ddevsr@users.noreply.github.com>
2023-09-18 16:22:18 +0700

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,66 +2,91 @@
 
 declare(strict_types=1);
 
-/**
- * This file is part of PHPDevsr/Minifyku.
- *
- * (c) 2023 Denny Septian Panggabean <xamidimura@gmail.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
+use Rector\CodeQuality\Rector\Assign\CombinedAssignRector;
 use Rector\CodeQuality\Rector\BooleanAnd\SimplifyEmptyArrayCheckRector;
+use Rector\CodeQuality\Rector\BooleanNot\ReplaceMultipleBooleanNotRector;
+use Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector;
 use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector;
+use Rector\CodeQuality\Rector\Concat\JoinStringConcatRector;
+use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector;
 use Rector\CodeQuality\Rector\Expression\InlineIfToExplicitIfRector;
-use Rector\CodeQuality\Rector\For_\ForToForeachRector;
+use Rector\CodeQuality\Rector\Expression\TernaryFalseExpressionToIfRector;
 use Rector\CodeQuality\Rector\Foreach_\UnusedForeachValueToArrayKeysRector;
-use Rector\CodeQuality\Rector\FuncCall\AddPregQuoteDelimiterRector;
+use Rector\CodeQuality\Rector\FuncCall\ArrayMergeOfNonArraysToSimpleArrayRector;
 use Rector\CodeQuality\Rector\FuncCall\ChangeArrayPushToArrayAssignRector;
 use Rector\CodeQuality\Rector\FuncCall\SimplifyRegexPatternRector;
 use Rector\CodeQuality\Rector\FuncCall\SimplifyStrposLowerRector;
 use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\CodeQuality\Rector\If_\CombineIfRector;
+use Rector\CodeQuality\Rector\If_\ConsecutiveNullCompareReturnsToNullCoalesceQueueRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfNotNullReturnRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfNullableReturnRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\CodeQuality\Rector\LogicalAnd\AndAssignsToSeparateLinesRector;
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
+use Rector\CodeQuality\Rector\NotEqual\CommonNotEqualRector;
+use Rector\CodeQuality\Rector\Ternary\ArrayKeyExistsTernaryThenValueToCoalescingRector;
+use Rector\CodeQuality\Rector\Ternary\NumberCompareToMaxFuncCallRector;
+use Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector;
 use Rector\CodeQuality\Rector\Ternary\UnnecessaryTernaryExpressionRector;
 use Rector\CodingStyle\Rector\ClassMethod\FuncGetArgsToVariadicParamRector;
 use Rector\CodingStyle\Rector\ClassMethod\MakeInheritedMethodVisibilitySameAsParentRector;
 use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\DeadCode\Rector\Array_\RemoveDuplicatedArrayKeyRector;
+use Rector\DeadCode\Rector\Assign\RemoveDoubleAssignRector;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\BooleanAnd\RemoveAndTrueRector;
+use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
+use Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateClassConstantRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
-use Rector\DeadCode\Rector\MethodCall\RemoveEmptyMethodCallRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector;
+use Rector\DeadCode\Rector\ConstFetch\RemovePhpVersionIdCheckRector;
+use Rector\DeadCode\Rector\Expression\RemoveDeadStmtRector;
+use Rector\DeadCode\Rector\Expression\SimplifyMirrorAssignRector;
+use Rector\DeadCode\Rector\Plus\RemoveDeadZeroAndOneOperationRector;
+use Rector\DeadCode\Rector\Return_\RemoveDeadConditionAboveReturnRector;
+use Rector\DeadCode\Rector\TryCatch\RemoveDeadTryCatchRector;
 use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
 use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
 use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
-use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php73\Rector\FuncCall\StringifyStrNeedlesRector;
-use Rector\PHPUnit\Rector\Class_\AnnotationWithValueToAttributeRector;
-use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
 use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
-use Rector\PSR4\Rector\FileWithoutNamespace\NormalizeNamespaceByPSR4ComposerAutoloadRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_74,
-        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
-        PHPUnitSetList::PHPUNIT_80,
+        SetList::CODE_QUALITY,
+        SetList::STRICT_BOOLEANS,
+        LevelSetList::UP_TO_PHP_82,
     ]);
 
-    $rectorConfig->parallel();
+    $rectorConfig->parallel(120, 16, 15);
 
     // The paths to refactor (can also be supplied with CLI arguments)
     $rectorConfig->paths([
-        __DIR__ . '/src/',
-        __DIR__ . '/tests/',
+        __DIR__ . '/app',
     ]);
 
     // Include Composer's autoload - required for global execution, remove if running locally
@@ -79,47 +104,72 @@ return static function (RectorConfig $rectorConfig): void {
     }
 
     // Set the target version for refactoring
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->phpVersion(PhpVersion::PHP_82);
 
     // Auto-import fully qualified class names
     $rectorConfig->importNames();
 
     // Are there files or rules you need to skip?
     $rectorConfig->skip([
-        __DIR__ . '/src/Views',
+        __DIR__ . '/app/Common.php',
+        __DIR__ . '/app/Config',
+        __DIR__ . '/app/Database',
+        __DIR__ . '/app/Language',
+        __DIR__ . '/app/Views',
 
         JsonThrowOnErrorRector::class,
         StringifyStrNeedlesRector::class,
 
         // Note: requires php 8
         RemoveUnusedPromotedPropertyRector::class,
-        AnnotationWithValueToAttributeRector::class,
-
-        // Ignore tests that might make calls without a result
-        RemoveEmptyMethodCallRector::class => [
-            __DIR__ . '/tests',
-        ],
-
-        // Ignore files that should not be namespaced to their folder
-        NormalizeNamespaceByPSR4ComposerAutoloadRector::class => [
-            __DIR__ . '/src/Helpers',
-            __DIR__ . '/src/Language',
-            __DIR__ . '/tests/_support',
-        ],
 
         // May load view files directly when detecting classes
         StringClassNameToClassConstantRector::class,
-
-        // May be uninitialized on purpose
-        AddDefaultValueForUndefinedVariableRector::class,
     ]);
     // auto import fully qualified class names
     $rectorConfig->importNames();
 
+    $rectorConfig->rule(RemoveDeadZeroAndOneOperationRector::class);
+    $rectorConfig->rule(InlineArrayReturnAssignRector::class);
+    $rectorConfig->rule(LogicalToBooleanRector::class);
+    $rectorConfig->rule(AndAssignsToSeparateLinesRector::class);
+    $rectorConfig->rule(SwitchNegatedTernaryRector::class);
+    $rectorConfig->rule(ArrayKeyExistsTernaryThenValueToCoalescingRector::class);
+    $rectorConfig->rule(TernaryFalseExpressionToIfRector::class);
+    $rectorConfig->rule(UseIdenticalOverEqualWithSameTypeRector::class);
+    $rectorConfig->rule(ConsecutiveNullCompareReturnsToNullCoalesceQueueRector::class);
+    $rectorConfig->rule(ExplicitBoolCompareRector::class);
+    $rectorConfig->rule(SimplifyIfNullableReturnRector::class);
+    $rectorConfig->rule(SimplifyIfNotNullReturnRector::class);
+    $rectorConfig->rule(ArrayMergeOfNonArraysToSimpleArrayRector::class);
+    $rectorConfig->rule(SimplifyDeMorganBinaryRector::class);
+    $rectorConfig->rule(CommonNotEqualRector::class);
+    $rectorConfig->rule(SimplifyEmptyCheckOnEmptyArrayRector::class);
+    $rectorConfig->rule(JoinStringConcatRector::class);
+    $rectorConfig->rule(SimplifyMirrorAssignRector::class);
+    $rectorConfig->rule(RemoveDeadStmtRector::class);
+    $rectorConfig->rule(RemovePhpVersionIdCheckRector::class);
+    $rectorConfig->rule(RemoveConcatAutocastRector::class);
+    $rectorConfig->rule(RemoveUnusedPrivateMethodRector::class);
+    $rectorConfig->rule(RemoveUnusedPrivateMethodParameterRector::class);
+    $rectorConfig->rule(RemoveUnusedConstructorParamRector::class);
+    $rectorConfig->rule(RemoveEmptyClassMethodRector::class);
+    $rectorConfig->rule(RemoveUselessParamTagRector::class);
+    $rectorConfig->rule(RemoveUnusedPrivateClassConstantRector::class);
+    $rectorConfig->rule(RemoveAndTrueRector::class);
+    $rectorConfig->rule(RecastingRemovalRector::class);
+    $rectorConfig->rule(RemoveDeadTryCatchRector::class);
+    $rectorConfig->rule(ReplaceMultipleBooleanNotRector::class);
+    $rectorConfig->rule(CombinedAssignRector::class);
+    $rectorConfig->rule(NumberCompareToMaxFuncCallRector::class);
+    $rectorConfig->rule(RemoveDeadConditionAboveReturnRector::class);
+    $rectorConfig->rule(RemoveUselessReturnTagRector::class);
+    $rectorConfig->rule(RemoveDuplicatedArrayKeyRector::class);
+    $rectorConfig->rule(RemoveDoubleAssignRector::class);
+    $rectorConfig->rule(RemoveUnusedVariableAssignRector::class);
     $rectorConfig->rule(SimplifyUselessVariableRector::class);
     $rectorConfig->rule(RemoveAlwaysElseRector::class);
     $rectorConfig->rule(CountArrayToEmptyArrayComparisonRector::class);
-    $rectorConfig->rule(ForToForeachRector::class);
     $rectorConfig->rule(ChangeNestedForeachIfsToEarlyContinueRector::class);
     $rectorConfig->rule(ChangeIfElseValueAssignToEarlyReturnRector::class);
     $rectorConfig->rule(SimplifyStrposLowerRector::class);
@@ -132,13 +182,15 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(UnusedForeachValueToArrayKeysRector::class);
     $rectorConfig->rule(ChangeArrayPushToArrayAssignRector::class);
     $rectorConfig->rule(UnnecessaryTernaryExpressionRector::class);
-    $rectorConfig->rule(AddPregQuoteDelimiterRector::class);
     $rectorConfig->rule(SimplifyRegexPatternRector::class);
     $rectorConfig->rule(FuncGetArgsToVariadicParamRector::class);
+    $rectorConfig->rule(RemoveUnusedVariableInCatchRector::class);
+    $rectorConfig->rule(ChangeSwitchToMatchRector::class);
+    $rectorConfig->rule(FirstClassCallableRector::class);
     $rectorConfig->rule(MakeInheritedMethodVisibilitySameAsParentRector::class);
     $rectorConfig->rule(SimplifyEmptyArrayCheckRector::class);
-    $rectorConfig->rule(NormalizeNamespaceByPSR4ComposerAutoloadRector::class);
     $rectorConfig->rule(StringClassNameToClassConstantRector::class);
     $rectorConfig->rule(PrivatizeFinalClassPropertyRector::class);
     $rectorConfig->rule(CompleteDynamicPropertiesRector::class);
+    $rectorConfig->rule(Utf8DecodeEncodeToMbConvertEncodingRector::class);
 };
